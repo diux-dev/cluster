3c71da8c2a093f54de15a71556a2d1fd4a78370c
Yaroslav Bulatov <yaroslavvb@gmail.com>
2017-12-18 18:05:41 -0800

    Fix vpc_tool to succeed in deleting vpc’s with dependencies

--- vpc_tool.py
+++ vpc_tool.py
@@ -4,6 +4,8 @@ import boto3
 import sys
 import os
 
+import common_resources as c
+
 def _get_name(tags):
   """Helper utility to extract name out of tags dictionary.
       [{'Key': 'Name', 'Value': 'nexus'}] -> 'nexus'
@@ -50,15 +52,39 @@ def main():
     assert len(sys.argv) == 3
     
     assert 'AWS_DEFAULT_REGION' in os.environ
-    client = _create_ec2_client()
-    
+    client = c.create_ec2_client()
+    ec2 = c.create_ec2_resource()
     response = client.describe_vpcs()
     for vpc_response in response['Vpcs']:
       vpc_name = _get_name(vpc_response.get('Tags', []))
-      vpc_id = vpc_response['VpcId']
+      vpc = ec2.Vpc(vpc_response['VpcId'])
       if vpc_name == sys.argv[2]:
-        print("Deleting VPC name=%s, id=%s"%(vpc_name, vpc_id))
-        client.delete_vpc(VpcId=vpc_id)
+        print("Deleting VPC name=%s, id=%s"%(vpc_name, vpc.id))
+        
+        for subnet in vpc.subnets.all():
+          print("Deleting subnet %s" % (subnet.id))
+          assert c.is_good_response(subnet.delete())
+
+        for gateway in vpc.internet_gateways.all():
+          print("Deleting gateway %s" % (gateway.id))
+          assert c.is_good_response(gateway.detach_from_vpc(VpcId=vpc.id))
+          assert c.is_good_response(gateway.delete())
+
+        for security_group in vpc.security_groups.all():
+          try:
+            assert c.is_good_response(security_group.delete())
+          except Exception as e:
+            print("Failed with "+str(e))
+            
+        for route_table in vpc.route_tables.all():
+          print("Deleting route table %s" % (route_table.id))
+          try:
+            assert c.is_good_response(route_table.delete())
+          except Exception as e:
+            print("Failed with "+str(e))
+          
+        if c.is_good_response(client.delete_vpc(VpcId=vpc.id)):
+          print("Succeeded deleting VPC ", vpc.id)
 
 if __name__=='__main__':
   main()
