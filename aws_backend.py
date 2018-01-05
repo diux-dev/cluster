e4ed87a6b30b79b422de99e69ca8fdeca6ad1534
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-01-04 20:10:19 -0800

    commit aws_backend

---
+++ aws_backend.py
@@ -0,0 +1,414 @@
+# AWS implementation of backend.py
+
+# TODO: fix remote_fn must be absolute for uploading with check_with_existing
+import glob
+import os
+import shlex
+import sys
+import time
+
+import portpicker
+
+import backend
+import util as u
+
+TASKDIR_PREFIX='/tmp/tasklogs'
+LOGDIR_PREFIX='/efs/runs'
+TIMEOUT_SEC=5
+MAX_RETRIES = 10
+DEFAULT_PORT=3000
+
+def make_run(name, **kwargs):
+  return Run(name, **kwargs)
+
+
+
+class Run(backend.Run):
+  def __init__(self, name, **kwargs):
+    self.name = name
+    self.kwargs = kwargs
+
+  # TODO: get rid of linux type (only login username)
+  def make_job(self, role_name, num_tasks, **kwargs):
+    assert num_tasks>=0
+
+    # TODO: document launch parameters
+    job_name = u.format_job_name(self.name, role_name)
+    instances = u.lookup_aws_instances(job_name)
+    kwargs = u.merge_kwargs(kwargs, self.kwargs)
+    ami = kwargs['ami']
+    instance_type = kwargs['instance_type']
+    availability_zone = kwargs['availability_zone']
+    placement_group = kwargs.get('placement_group', '')
+    install_script = kwargs['install_script']
+    linux_type = kwargs.get('linux_type', 'ubuntu')
+
+    # TODO: also make sure instance type is the same
+    if instances:
+      assert len(instances) == num_tasks, ("Found job with same name, but number of tasks %d doesn't match requested %d, kill job manually." % (len(instances), num_tasks))
+      print("Found existing job "+job_name)
+    else:
+      print("Launching new job %s into VPC %s" %(job_name, u.RESOURCE_NAME))
+
+      security_group = u.get_security_group_dict()[u.RESOURCE_NAME]
+      keypair = u.get_keypair_dict()[u.RESOURCE_NAME]
+      vpc = u.get_vpc_dict()[u.RESOURCE_NAME]
+      subnet = u.get_subnet_dict(vpc)[availability_zone]
+      ec2 = u.create_ec2_resource()
+      u.maybe_create_placement_group(placement_group)
+
+      self.log("Requesting %d %s" %(num_tasks, instance_type))
+
+      args = {'ImageId': ami,
+              'InstanceType': instance_type,
+              'MinCount': num_tasks,
+              'MaxCount': num_tasks,
+              'KeyName': keypair.name}
+
+      # network setup
+      args['NetworkInterfaces'] = [{'SubnetId': subnet.id,
+                                    'DeviceIndex': 0,
+                                    'AssociatePublicIpAddress': True,
+                                    'Groups': [security_group.id]}]
+      
+      placement_arg = {'AvailabilityZone': availability_zone}
+      if placement_group: placement_arg['GroupName'] = placement_group
+      args['Placement'] = placement_arg
+
+      instances = ec2.create_instances(**args)
+      assert len(instances) == num_tasks
+
+      # assign proper names to tasks
+      for instance in instances:
+        while True:
+          try:
+            # sometimes get "An error occurred (InvalidInstanceID.NotFound)"
+            task_name = u.format_task_name(self.name, role_name,
+                                           instance.ami_launch_index)
+            # TODO: use instance.create_tags instead like in create_resources.py
+            ec2.create_tags(Resources=[instance.id],
+                            Tags=u.make_name(task_name))
+            break
+          except Exception as e:
+            self.log("create_tags failed with %s, retrying in %d seconds"%(
+              str(e), TIMEOUT_SEC))
+            time.sleep(TIMEOUT_SEC)
+
+    job = Job(self, job_name, instances=instances,
+              install_script=install_script,
+              linux_type=linux_type)
+    return job
+
+  @property
+  def logdir(self):
+    return LOGDIR_PREFIX+'/'+self.name
+
+
+class Job(backend.Job):
+  # TODO: get rid of linux_type
+  def __init__(self, run, name, instances, install_script=None,
+               linux_type=None):
+    self._run = run
+    self.name = name
+
+
+    self._run_command_available = False, "Have you done wait_until_ready?"
+    
+    # initialize list of tasks, in order of AMI launch index
+    self.tasks = [None]*len(instances)
+    for instance in instances:
+      task_id = instance.ami_launch_index
+      task = Task(instance, self, task_id, install_script=install_script,
+                             linux_type=linux_type)
+      self.tasks[task_id] = task
+
+  def _initialize(self):
+    for task in self.tasks:
+      task._initialize()
+
+  # todo: rename to initialize
+  def wait_until_ready(self):
+    """Waits until all tasks in the job are available and initialized."""
+    for task in self.tasks:
+      task.wait_until_ready()
+      # todo: initialization should start async in constructor instead of here
+
+
+class Task(backend.Task):
+  # TODO: replace linux_type with username
+  def __init__(self, instance, job, task_id, install_script=None,
+               linux_type=None):
+    self.initialize_called = False
+    self.instance = instance
+    self.job = job
+    self.id = task_id
+    self.install_script = install_script
+    self._run_counter = 0
+    
+    self.initialized = False
+
+    # scratch is client-local space for temporary files
+    self.scratch = "{}/{}.{}.{}.{}/scratch".format(TASKDIR_PREFIX,
+                                                   job._run.name,
+                                                   job.name, self.id,
+                                                   0) # u.now_micros())
+    self.remote_scratch = '/tmp/tmux'
+    self.log("Creating remote scratch dir %s", self.scratch)
+    self._ossystem('rm -Rf '+self.scratch)  # TODO: don't delete this?
+    self._ossystem('mkdir -p '+self.scratch)
+    #    os.chdir(self.scratch)
+
+    # todo: create taskdir
+    self.connect_instructions = "waiting for initialize()"
+    self.keypair_fn = u.get_keypair_fn(u.RESOURCE_NAME)
+
+    # username to use to ssh into instances
+    # ec2-user or ubuntu
+    if linux_type == 'ubuntu':
+      self.username = 'ubuntu'
+    elif linux_type == 'amazon':
+      self.username = 'ec2-user'
+    else:
+      assert False, "Unknown linux type '%s', expected 'ubuntu' or 'amazon'."
+
+    self.taskdir = '/home/'+self.username
+
+  # todo: replace with file_read
+  def _is_initialized_file_present(self):
+    self.log("Checking for initialized file")
+    try:
+      # TODO: either get rid of /tmp or taskdir
+      return 'ok' in self.file_read('/tmp/is_initialized')
+    except:
+      return False
+
+
+  def _setup_tmux(self):
+    self._run_ssh('tmux kill-session -t tmux')
+    self._run_ssh('tmux new-session -s tmux -n 0 -d')
+    self._run_command_available = True
+
+  def _mount_efs(self):
+    self.log("Mounting EFS")
+    region = u.get_region()
+    efs_id = u.get_efs_dict()[u.RESOURCE_NAME]
+    dns = "{efs_id}.efs.{region}.amazonaws.com".format(**locals())
+    self.run('sudo mkdir -p /efs')
+    self.run('sudo chmod 777 /efs')
+    # ignore error on remount
+    self.run("sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 %s:/ /efs"%(dns,), ignore_errors=True) 
+
+  def _initialize(self):
+    """Tries to initialize the task."""
+
+    self.log("Running initialize")
+    self.initialize_called = True
+    while True:
+      # TODO: this try/except is wrong, should check for None instead
+      try:
+        public_ip = self.public_ip
+        break
+      except:
+        self.log("no public IP, retrying in %d seconds"%(TIMEOUT_SEC))
+      time.sleep(TIMEOUT_SEC)
+
+    while True:
+      self.ssh_client = u.ssh_to_host(self.public_ip, self.keypair_fn,
+                                      self.username)
+      if self.ssh_client is None:
+        self.log("SSH into %s:%s failed, retrying in %d seconds" %(self.job.name, self.id,TIMEOUT_SEC))
+        time.sleep(TIMEOUT_SEC)
+      else:
+        break
+
+    # todo: install tmux
+    self._setup_tmux()
+    self.run('mkdir -p '+self.remote_scratch)
+    self._mount_efs()
+    
+    # run initialization commands here
+    if self._is_initialized_file_present():
+      self.log("reusing previous initialized state")
+    else:
+      self.log("running install script")
+
+      self.install_script+='\necho ok > /tmp/is_initialized\n'
+      self.file_write('install.sh', u._add_echo(self.install_script))
+      self.run('bash -e install.sh') # fail on errors
+
+    assert self._is_initialized_file_present()
+
+    self.connect_instructions = """
+ssh -i %s -o StrictHostKeyChecking=no %s@%s
+tmux a
+""".strip() % (self.keypair_fn, self.username, self.public_ip)
+    self.log("Initialize complete")
+
+
+  # todo: rename wait_until_ready to wait_until_initialized
+  def wait_until_ready(self):
+    if not self.initialize_called:
+      self._initialize()
+    while not self._is_initialized_file_present():
+      self.log("wait_until_ready: Not initialized, retrying in %d seconds"%(TIMEOUT_SEC))
+      time.sleep(TIMEOUT_SEC)
+
+
+  # TODO: dedup with tmux_backend.py?
+  def _upload_handler(self, line):
+    """Handle following types of commands.
+
+    Individual files, ie
+    %upload file.txt
+
+    Glob expressions, ie
+    %upload *.py"""
+
+    fname = fname.replace("~", os.environ["HOME"])
+
+    toks = line.split()
+    assert len(toks) == 2
+    assert toks[0] == '%upload'
+
+    for fn in glob.glob(toks[1]):
+      self.upload(fn)
+
+
+  def upload(self, local_fn, remote_fn=None, skip_existing=False):
+    """Uploads file to remote instance. If location not specified, dumps it
+    in default directory."""
+    # TODO: self.ssh_client is sometimes None
+    self.log('uploading '+local_fn)
+    sftp = self.ssh_client.open_sftp()
+    if remote_fn is None:
+      remote_fn = os.path.basename(local_fn)
+    if skip_existing and self.file_exists(remote_fn):
+      self.log("Remote file %s exists, skipping"%(remote_fn,))
+    else:
+      sftp.put(local_fn, remote_fn)
+
+
+  def download(self, remote_fn, local_fn=None):
+    # TODO: self.ssh_client is sometimes None
+    self.log("downloading %s"%(remote_fn))
+    sftp = self.ssh_client.open_sftp()
+    if local_fn is None:
+      local_fn = os.path.basename(local_fn)
+      self.log("downloading %s to %s"%(remote_fn, local_fn))
+    sftp.get(remote_fn, local_fn)
+
+
+  def file_exists(self, remote_fn):
+    # since ssh commands don't have state, can't rely on current directory
+    if not remote_fn.startswith('/'):
+      remote_fn = self.taskdir + '/'+remote_fn
+    assert remote_fn.startswith('/'), "Remote fn must be absolute"
+    stdin, stdout, stderr = self.ssh_client.exec_command('stat '+remote_fn,
+                                                    get_pty=True)
+    stdout_bytes = stdout.read()
+    stdout_str = stdout_bytes.decode()  # AWS linux uses unicode quotes
+    stderr_bytes = stderr.read()
+    stderr_str = stderr_bytes.decode()
+    if 'No such file' in stdout_str:
+      return False
+    else:
+      return True
+  
+  def file_write(self, remote_fn, contents):
+    tmp_fn = self.scratch+'/'+str(u.now_micros())
+    open(tmp_fn, 'w').write(contents)
+    self.upload(tmp_fn, remote_fn)
+  
+
+  def file_read(self, remote_fn):
+    self.log("file_read")
+    tmp_fn = self.scratch+'/'+str(u.now_micros())
+    self.download(remote_fn, tmp_fn)
+    return open(tmp_fn).read()
+
+  def _run_ssh(self, cmd):
+    """Runs given cmd in the task using current SSH session, returns
+    stdout/stderr as strings. Because it blocks until cmd is done, use it for
+    short cmds.
+   
+    Also, because this can run before task is initialized, use it
+    for running initialization commands in sequence.
+
+    This is a barebones method to be used during initialization that have
+    minimal dependencies (no tmux)
+    """
+    self.log("run_ssh: %s"%(cmd,))
+    stdin, stdout, stderr = self.ssh_client.exec_command(cmd, get_pty=True)
+    stdout_str = stdout.read().decode()
+    stderr_str = stderr.read().decode()
+    self.log("run_ssh returned: " + stdout_str)
+    return stdout_str, stderr_str
+
+
+  def run(self, cmd, sync=True, ignore_errors=False,
+          max_wait_sec=600, check_interval=1):
+    """Runs command in tmux session. No need for multiple tmux sessions per
+    task, so assume tmux session/window is always called tmux:0"""
+
+    assert self._run_command_available
+    cmd = cmd.strip()
+    
+    self._run_counter+=1
+    self.log("tmux> %s", cmd)
+
+    # todo: match logic in tmux_session (upload magic handling done
+    # in init instead of run)
+    if cmd.startswith('%upload'):
+      self._upload_handler(cmd)
+      return
+
+    # locking to wait for command to finish
+    ts = str(u.now_micros())
+    cmd_fn_out = self.remote_scratch+'/'+str(self._run_counter)+'.'+ts+'.out'
+
+    modified_cmd = '%s; echo $? > %s'%(cmd, cmd_fn_out)
+    tmux_window = 'tmux:0'
+    tmux_cmd = "tmux send-keys -t {} {} Enter".format(tmux_window,
+                                                        shlex.quote(modified_cmd))
+    self._run_ssh(tmux_cmd)
+    if not sync:
+      return
+    
+    start_time = time.time()
+
+    while True:
+      if time.time() - start_time > max_wait_sec:
+        assert False, "Timeout %s exceeded for %s" %(max_wait_sec, cmd)
+      if not self.file_exists(cmd_fn_out):
+        self.log("waiting %s for %s"%(check_interval, cmd))
+        time.sleep(check_interval)
+        continue
+    
+      contents = self.file_read(cmd_fn_out)
+      # if empty wait a bit to allow for race condition
+      if len(contents) == 0:
+        time.sleep(check_interval)
+        contents = task.file_read(cmd_fn_out)
+
+      contents = contents.strip()
+      if contents != '0':
+        if not ignore_errors:
+          assert False, "Command %s returned status %s"%(cmd, contents)
+        else:
+          self.log("Warning: command %s returned status %s"%(cmd, contents))
+      break    
+
+   
+  @property
+  def public_ip(self):
+    self.instance.load()
+    return self.instance.public_ip_address
+
+  @property
+  def port(self):
+    return DEFAULT_PORT
+
+  @property
+  def ip(self):  # private ip
+    self.instance.load()
+    return self.instance.private_ip_address
