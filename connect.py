59bb5d1881727a51bc15bed4047921997e2b0e8b
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-01-05 09:37:36 -0800

    minor fix

--- connect.py
+++ connect.py
@@ -47,6 +47,9 @@ def toseconds(dt):
 def main():
   fragment = args.fragment
 
+  # TODO: prevent CTRL+c/CTRL+d from killing session
+  print("Launching into TMUX session, use CTRL+b d to exit")
+
   region = os.environ['AWS_DEFAULT_REGION']
   client = boto3.client('ec2', region_name=region)
   ec2 = boto3.resource('ec2', region_name=region)
