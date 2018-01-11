2fc37d361a0d14cd5d4bf5cb0c7e0011a2469f3f
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-01-11 11:51:22 -0800

    fix

--- connect_helper.sh
+++ connect_helper.sh
@@ -3,4 +3,6 @@
 # See
 # https://stackoverflow.com/questions/7114990/pseudo-terminal-will-not-be-allocated-because-stdin-is-not-a-terminal
 # https://stackoverflow.com/questions/1376016/python-subprocess-with-heredocs
-ssh -t -i $1 -o StrictHostKeyChecking=no $2@$3 tmux a
+export cmd="ssh -t -i $1 -o StrictHostKeyChecking=no $2@$3 tmux a"
+echo $cmd
+$cmd
