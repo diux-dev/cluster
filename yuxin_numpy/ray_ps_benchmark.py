f20b0535a0a2d8bc704f802138844a53da1d9f9e
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-02-21 12:21:43 -0800

    clean-up ray benchmark

--- yuxin_numpy/ray_ps_benchmark.py
+++ yuxin_numpy/ray_ps_benchmark.py
@@ -1,6 +1,3 @@
-# To run the example, use a command like the following.
-#
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -19,22 +16,18 @@ parser.add_argument("--num-parameter-servers", default=1, type=int,
                     help="The number of parameter servers to use.")
 parser.add_argument("--dim", default=25*1000*1000, type=int,
                     help="The number of parameters.")
-parser.add_argument("--redis-address", default=None, type=str,
-                    help="The Redis address of the cluster.")
+parser.add_argument("--skip-ray", default=0, type=int,
+                    help="skip ray and add directly in numpy")
 
 args = parser.parse_args()
 
-
-# TODO(rkn): This is a placeholder.
 class CNN(object):
     def __init__(self, dim):
         self.dim = dim
-
-    def get_gradients(self):
-        return np.ones(self.dim, dtype=np.float32)
-
-    def set_weights(self, weights):
-        pass
+        self.grads = np.ones(self.dim, dtype=np.float32)
+        
+    def get_gradients(self, params):
+        return self.grads
 
 
 @ray.remote
@@ -42,53 +35,46 @@ class ParameterServer(object):
     def __init__(self, dim):
         self.params = np.zeros(dim)
 
-    def update_and_get_new_weights(self, gradients):
+    def update_and_get_new_params(self, gradients):
         self.params += gradients
-
-    def ip(self):
-        return ray.services.get_node_ip_address()
+        return self.params
 
 @ray.remote
 class Worker(object):
-    def __init__(self, num_ps, dim):
+    def __init__(self, dim):
         self.net = CNN(dim)
-        self.num_ps = num_ps
 
-    @ray.method(num_return_vals=args.num_parameter_servers)
-    def compute_gradient(self, weights):
-        all_weights = weights
-        self.net.set_weights(all_weights)
-        gradient = self.net.get_gradients()
-        return gradient
-
-    def ip(self):
-        return ray.services.get_node_ip_address()
+    def compute_gradient(self, params):
+        return self.net.get_gradients(params)
 
 
-if __name__ == "__main__":
-  if args.redis_address is None:
-    ray.init(num_gpus=args.num_parameter_servers + args.num_workers)
-  else:
-    ray.init(redis_address=args.redis_address)
-
-
+def main():
   # initial parameter value
-  split_weights = np.zeros(args.dim, dtype=np.float32)
+  params = np.zeros(args.dim, dtype=np.float32)
 
-  # worker and parameter server
-  ps = ParameterServer.remote(split_weights.size)
-  worker = Worker.remote(args.num_parameter_servers, args.dim)
+  if not args.skip_ray:
+    ray.init()
+
+    ps = ParameterServer.remote(args.dim)
+    worker = Worker.remote(args.dim)
+
+  RUN_RAY = True
+  net = CNN(args.dim)
 
   for i in range(100):
     t1 = time.time()
 
-    gradients = worker.compute_gradient.remote(split_weights)
-    ray.wait([gradients])
+    if args.skip_ray:
+      params += net.get_gradients(params)
+      t2 = time.time()
+    else:
+      grads = worker.compute_gradient.remote(params)
+      ray.wait([grads])
 
-    t2 = time.time()
+      t2 = time.time()
 
-    split_weights = ps.update_and_get_new_weights.remote(gradients)
-    ray.wait([split_weights])
+      params = ps.update_and_get_new_params.remote(grads)
+      ray.wait([params])
 
     t3 = time.time()
     t1ms = 1000*t1
@@ -96,3 +82,6 @@ if __name__ == "__main__":
     t3ms = 1000*t3
     print("elapsed times: total %4.2f worker update %4.2f ps update %4.2f" %( t3ms - t1ms, t2ms - t1ms, t3ms - t2ms))
         
+
+if __name__ == "__main__":
+  main()
