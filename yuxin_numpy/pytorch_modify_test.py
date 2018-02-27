f74655752ba11551fa0253a44376bbcd1ddb821f
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-02-27 10:33:58 -0800

    pytorch changing read-only attribute of numpy array

---
+++ yuxin_numpy/pytorch_modify_test.py
@@ -0,0 +1,29 @@
+import argparse
+import numpy as np
+import ray
+import time
+import torch
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--size-mb", default=1000, type=int,
+                    help="size of data in MBs")
+args = parser.parse_args()
+
+import ray
+ray.init(num_workers=0)
+
+@ray.remote
+def f():
+  return np.ones((args.size_mb * 250*1000,), np.float32)
+
+
+arr = ray.get(f.remote())
+assert arr[0] == 1.0
+
+# uncomment to get fail with "ValueError: output array is read-only"
+# arr+=1
+
+arr = torch.from_numpy(arr)
+arr+=1
+assert arr[0] == 2.0
+
