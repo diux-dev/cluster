1a35c215537afd535969c4e61ade13198ede1665
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-07-07 05:28:25 -0700

    Removing CheckDataManager. Also removing use_val_sampler flag. Always using distributed val sampler as it reflects the correct accuracy now

--- pytorch/training/train_imagenet_nv.py
+++ pytorch/training/train_imagenet_nv.py
@@ -196,7 +196,7 @@ def create_validation_set(valdir, batch_size, target_size, use_ar):
     val_sampler = ValDistSampler(list(range(len(val_dataset))))
     return val_dataset, val_sampler
 
-def get_loaders(traindir, valdir, sz, bs, val_bs=None, use_val_sampler=True, min_scale=0.08):
+def get_loaders(traindir, valdir, sz, bs, val_bs=None, min_scale=0.08):
     val_bs = val_bs or bs
     train_dataset = datasets.ImageFolder(
         traindir, transforms.Compose([
@@ -210,12 +210,11 @@ def get_loaders(traindir, valdir, sz, bs, val_bs=None, use_val_sampler=True, min
         num_workers=args.workers, pin_memory=True, collate_fn=fast_collate, 
         sampler=train_sampler)
 
-    use_val_ar = args.val_ar and (not use_val_sampler)
-    val_dataset, val_sampler = create_validation_set(valdir, val_bs, sz, use_ar=use_val_ar)
+    val_dataset, val_sampler = create_validation_set(valdir, val_bs, sz, use_ar=args.val_ar)
     val_loader = torch.utils.data.DataLoader(
         val_dataset, batch_size=val_bs, shuffle=False,
         num_workers=args.workers, pin_memory=True, collate_fn=fast_collate, 
-        sampler=val_sampler if use_val_sampler else None)
+        sampler=val_sampler)
     return train_loader,val_loader,train_sampler,val_sampler
 
 
@@ -282,9 +281,6 @@ class DataManager():
             self.load_data('', args.batch_size, 224)
         if epoch==int(args.epochs*0.92+0.5)+args.warmup:
             self.load_data('', 128, 288, min_scale=0.5)
-        if epoch==args.epochs+args.warmup-2:
-            self.load_data('', 128, 288, use_val_sampler=False, min_scale=0.5)
-
         if args.distributed:
             if self.trn_smp: self.trn_smp.set_epoch(epoch)
             if self.val_smp: self.val_smp.set_epoch(epoch)
@@ -315,51 +311,6 @@ class DataManager():
         gc.collect()
         torch.cuda.empty_cache()
 
-class CheckDataManager():
-    def __init__(self):
-        if args.small: self.load_data('-sz/160', args.batch_size, 128, use_val_sampler=False)
-        # else: self.load_data('-sz/320', args.batch_size, 224)
-        else: self.load_data('', args.batch_size, 224, use_val_sampler=False)
-        
-    def set_epoch(self, epoch):
-        if epoch==int(args.epochs*0.4+0.5)+args.warmup:
-            # self.load_data('-sz/320', args.batch_size, 224)
-            self.load_data('', args.batch_size, 224, use_val_sampler=False)
-        if epoch==int(args.epochs*0.92+0.5)+args.warmup:
-            self.load_data('', 128, 288, use_val_sampler=False, min_scale=0.5)
-        if epoch==args.epochs+args.warmup-2:
-            self.load_data('', 128, 288, use_val_sampler=False, min_scale=0.5)
-
-        if args.distributed:
-            if self.trn_smp: self.trn_smp.set_epoch(epoch)
-            if self.val_smp: self.val_smp.set_epoch(epoch)
-
-    def get_trn_iter(self):
-        # trn_iter = self.trn_iter
-        self.trn_iter = iter(self.trn_dl)
-        return self.trn_iter
-
-    def get_val_iter(self):
-        # val_iter = self.val_iter
-        self.val_iter = iter(self.val_dl)
-        return self.val_iter
-        
-    def load_data(self, dir_prefix, batch_size, image_size, **kwargs):
-        traindir = args.data+dir_prefix+'/train'
-        valdir = args.data+dir_prefix+'/validation'
-        self.trn_dl,self.val_dl,self.trn_smp,self.val_smp = get_loaders(traindir, valdir, bs=batch_size, sz=image_size, **kwargs)
-        self.trn_dl = DataPrefetcher(self.trn_dl)
-        self.val_dl = DataPrefetcher(self.val_dl)
-
-        self.trn_len = len(self.trn_dl)
-        self.val_len = len(self.val_dl)
-        # self.trn_iter = iter(self.trn_dl)
-        # self.val_iter = iter(self.val_dl)
-
-        # clear memory
-        gc.collect()
-        torch.cuda.empty_cache()
-        
 class Scheduler():
     def __init__(self, optimizer):
         self.optimizer = optimizer
@@ -493,7 +444,6 @@ def main():
         else: print("=> no checkpoint found at '{}'".format(args.resume))
 
     dm = DataManager()
-    check_dm = CheckDataManager()
     print("Created data loaders")
 
     if args.evaluate: return validate(dm.val_dl, len(dm.val_dl), model, criterion, epoch, start_time)
@@ -504,7 +454,6 @@ def main():
         estart = time.time()
         # adjust_learning_rate(optimizer, epoch)
         dm.set_epoch(epoch)
-        check_dm.set_epoch(epoch)
 
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=UserWarning)
@@ -512,7 +461,6 @@ def main():
 
         if args.prof: break
         prec5 = validate(dm.get_val_iter(), len(dm.val_dl), model, criterion, epoch, start_time)
-        prec5 = validate(check_dm.get_val_iter(), len(check_dm.val_dl), model, criterion, epoch, start_time)
 
         is_best = prec5 > best_prec5
         if args.local_rank == 0 and is_best:
