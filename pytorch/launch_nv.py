655f570ec198e844e26e3955d591604a303f8e8f
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-07-15 15:40:38 +0200

    match 16 machine schedule to match 8 machines

--- pytorch/launch_nv.py
+++ pytorch/launch_nv.py
@@ -133,10 +133,11 @@ x8ar_args = [
 # Current benchmark for 16x p3's - with Aspect Ratio Validatoin
 # python launch_nv.py --name yaro-friday-16 --num-tasks 16 --zone us-east-1c --params x16ar_args
 
+# Current benchmark for 8x p3's - with Aspect Ratio Validatoin
 x16ar_args = [
-  '--lr-sched', '0.14,0.47,0.78,0.95',
+  '--lr-sched', '0.14,0.43,0.75,0.94',
   '--resize-sched', '0.35,0.88',
-  '--epochs', 100,
+  '--epochs', 80,
   '--lr', 0.25 * 8,
   '--init-bn0',
   '--batch-size', 64,
