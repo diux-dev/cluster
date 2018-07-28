5b1ab0de8d764bcf3ab7a943bbee47b52fea052a
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-07-28 13:06:54 -0700

    Remove unnecessary dependencies
    This did not have any noticeable effect to iteration times, but makes start-up much faster
    
    Random time before: 143 ms
    Random time after: 141 ms
    
    Before: baseline-single-ami12.2018-07-28_09-36-50
    After: nofrills00.2018-07-28_12-52-58

--- pytorch/setup/setup_env_nv.sh
+++ pytorch/setup/setup_env_nv.sh
@@ -6,26 +6,26 @@
   
 echo 'Starting script'
 
-echo '> update'
+# echo '> update'
 # Turn off apt-get update for now, since using recent DLAMI 12
 # sudo apt-get update
 # sudo apt-get upgrade -y
 
-echo '> gdb'
-sudo apt install -y gdb
-echo '> nload'
-sudo apt install -y nload
-echo '> htop'
-sudo apt install -y htop
+# echo '> gdb'
+# sudo apt install -y gdb
+# echo '> nload'
+# sudo apt install -y nload
+# echo '> htop'
+# sudo apt install -y htop
 
 # Change nccl to 9-1
 sed -i -e 's/cuda-9.0/cuda-9.2/g' ~/.bashrc
 source ~/.bashrc
 
-echo '>pytorch'
-conda install pytorch torchvision cuda91 -c pytorch -y
-echo '>tqdm'
-conda install tqdm -y
+# echo '>pytorch'
+# conda install pytorch torchvision cuda91 -c pytorch -y
+# echo '>tqdm'
+# conda install tqdm -y
 
 # index file used to speed up evaluation
 echo '>indices'
@@ -34,9 +34,9 @@ wget --no-clobber https://s3.amazonaws.com/yaroslavvb/sorted_idxar.p
 popd
 
 # Installing through pip is faster than following this https://gist.github.com/soumith/01da3874bf014d8a8c53406c2b95d56b
-echo '>pillow'
-pip uninstall pillow -y
-CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
+# echo '>pillow'
+# pip uninstall pillow -y
+# CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
 
 
 # following GPU settings from below (not clear if helps) 
