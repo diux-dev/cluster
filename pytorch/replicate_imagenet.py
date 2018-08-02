f4ec3644fc9a15a4a209b148111125535cf74b57
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-08-01 19:33:08 -0700

    add offset in ebs replication script

--- pytorch/replicate_imagenet.py
+++ pytorch/replicate_imagenet.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # Script to initialize a set of high-performance volumes with ImageNet data
 # 
-# python replicate_imagenet.py --replicas 8 --zone=us-east-1c
-# Creates volumes: imagenet_00, imagenet_01, imagenet_02, etc
+# replicate_imagenet.py --replicas 8 --zone=us-east-1c
+# replicate_imagenet.py --replicas 8 --zone=us-east-1c --volume-offset=8
 #
-# This relies on snapshot with ImageNet data following structure as in
+# or
+#
+# replicate_imagenet.py --replicas 16 --zone=us-east-1c
+# Creates volumes: imagenet_00, imagenet_01, imagenet_02, ..., imagenet_15
+#
+# ImageNet data should follow structure as in
 # https://github.com/diux-dev/cluster/tree/master/pytorch#data-preparation
 # (paths replace ~/data with /)
 #
 # steps to create snapshot:
 # create blank volume (ec2.create_volume())
-# attach it to an existing instance with ImageNet under data 
+# attach it to an existing instance with ImageNet under data, then
 # sudo mkfs -t ext4 /dev/xvdf
 # mkdir data
 # sudo mount /dev/xvdf data
@@ -24,6 +29,7 @@ parser = argparse.ArgumentParser(description='launch')
 parser.add_argument('--zone', type=str, default='us-east-1c')
 parser.add_argument('--replicas', type=int, default=8)
 parser.add_argument('--snapshot', type=str, default='snap-0d37b903e01bb794a')
+parser.add_argument('--volume-offset', type=int, default=0, help='start numbering with this value')
 args = parser.parse_args()
 
 import os
@@ -45,7 +51,7 @@ def create_tags(name):
 if __name__=='__main__':
   ec2 = u.create_ec2_resource()
   print(f"Making {args.replicas} replicas in {args.zone}")
-  for i in range(args.replicas):
+  for i in range(args.volume_offset, args.replicas+args.volume_offset):
     vol = ec2.create_volume(Size=300, VolumeType='io1',
                       TagSpecifications=create_tags('imagenet_%02d'%(i)),
                       AvailabilityZone=args.zone,
