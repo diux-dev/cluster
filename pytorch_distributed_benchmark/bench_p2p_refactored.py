552bab8f0d627282f028a8dab9583373a45d17b9
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-08-18 09:45:41 -0700

    pytorch dist example in new interface

--- pytorch_distributed_benchmark/bench_p2p_refactored.py
+++ pytorch_distributed_benchmark/bench_p2p_refactored.py
@@ -9,11 +9,10 @@ import time
 
 import argparse
 
+PYTORCH_DIST_BACKEND = 'tcp'
 parser = argparse.ArgumentParser(description='launch')
 
 # launcher flags
-parser.add_argument('--backend', type=str, default='tcp',
-                    help='which PyTorch communication backend to use')
 parser.add_argument('--name', type=str, default='p2p',
                      help="name of the current run")
 parser.add_argument('--instance-type', type=str, default='t2.large',
@@ -39,6 +38,7 @@ parser.add_argument('--master-addr', type=str, default='127.0.0.1',
 parser.add_argument('--master-port', type=int, default=6006,
                     help='port of master node')
 
+import ncluster
 
 args = parser.parse_args()
 
@@ -52,7 +52,8 @@ def worker():
   print("Initializing distributed pytorch")
   os.environ['MASTER_ADDR'] = str(args.master_addr)
   os.environ['MASTER_PORT'] = str(args.master_port)
-  dist.init_process_group(args.backend, rank=args.rank, world_size=args.size)
+  dist.init_process_group(PYTORCH_DIST_BACKEND, rank=args.rank,
+                          world_size=args.size)
 
   for i in range(100):
     tensor = torch.ones(args.data_size_mb*250*1000)*(args.rank+1)
@@ -72,21 +73,10 @@ def worker():
 
 
 def launcher():
-  module_path=os.path.dirname(os.path.abspath(__file__))
-  sys.path.append(module_path+'/..')
-  import tmux_backend
-  import aws_backend
-  import util as u
-
-  if args.run_locally:
-    backend = tmux_backend
-  else:
-    u.maybe_create_resources()
-    backend = aws_backend
     
-  run = backend.make_run(args.name, ami_name=args.ami_name)
-  job = run.make_job('worker', instance_type=args.instance_type, num_tasks=2, use_placement_group=args.placement)
-  job.wait_until_ready()
+  job = ncluster.make_job('worker',
+                          instance_type=args.instance_type, num_tasks=2)
+  job.join()
 
   print("Job ready for connection, to connect to most recent task:")
   print("../connect "+args.name)
@@ -95,14 +85,11 @@ def launcher():
   print()
 
   job.upload(__file__)
-  if not args.run_locally: 
-    job.run('killall python || echo failed')  # kill previous run
-    
   
   job.run('source activate pytorch_p36')
   script_name = os.path.basename(__file__)
-  job.tasks[0].run_async(f'python {script_name} --internal-role=worker --rank=0 --size=2 --master-addr={job.tasks[0].ip} --backend={args.backend}')
-  job.tasks[1].run_async(f'python {script_name} --internal-role=worker --rank=1 --size=2 --master-addr={job.tasks[0].ip} --backend={args.backend}')
+  job.tasks[0].run_async(f'python {script_name} --internal-role=worker --rank=0 --size=2 --master-addr={job.tasks[0].ip}')
+  job.tasks[1].run_async(f'python {script_name} --internal-role=worker --rank=1 --size=2 --master-addr={job.tasks[0].ip}')
 
 def main():
   if args.internal_role == "launcher":
